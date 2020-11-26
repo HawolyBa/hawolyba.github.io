c54feced17f57e12971a06bc62f72cc85e57d738
hawolyba <hawolyba@gmail.com>
2020-11-26 21:21:43 +0100

    Updates

---
+++ precache-manifest.ef6097a910e53e1106197fe8b6aac0b0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a8eb08945841bb30e34bae501a28bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c0c6f12745331e77c55",
+    "url": "/static/css/2.b62e34e1.chunk.css"
+  },
+  {
+    "revision": "d2bfe57b20994ec43d50",
+    "url": "/static/css/main.b074e2b8.chunk.css"
+  },
+  {
+    "revision": "7c0c6f12745331e77c55",
+    "url": "/static/js/2.24b00d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24b00d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bfe57b20994ec43d50",
+    "url": "/static/js/main.87724537.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "28c67faf8d4df3f97ea3de75a14e3437",
+    "url": "/static/media/amazon.28c67faf.png"
+  },
+  {
+    "revision": "a41aa585ec25dd26b4b0fc6f34e526b9",
+    "url": "/static/media/cv.a41aa585.pdf"
+  },
+  {
+    "revision": "b95c8f744f24e7cd631c61af9232e98f",
+    "url": "/static/media/hdn.b95c8f74.png"
+  },
+  {
+    "revision": "454529bd29d97be706afd44f695285d6",
+    "url": "/static/media/ifocop.454529bd.jpg"
+  },
+  {
+    "revision": "cf7c3c85135072f39a484fa6a8fea067",
+    "url": "/static/media/instagram.cf7c3c85.png"
+  },
+  {
+    "revision": "766748e8b21e0c5d1a444e73b373b851",
+    "url": "/static/media/linternaute.766748e8.jpg"
+  },
+  {
+    "revision": "74aeb0509283f68449e4a881b721e105",
+    "url": "/static/media/logo-iicp.74aeb050.png"
+  },
+  {
+    "revision": "313f849ff7899156ce1bb7bbda25b640",
+    "url": "/static/media/logo.313f849f.png"
+  },
+  {
+    "revision": "0ff4c742757d3047f2b84237df7cf08f",
+    "url": "/static/media/netflix.0ff4c742.png"
+  },
+  {
+    "revision": "cf5c470f98250561d403fee713aa2897",
+    "url": "/static/media/office.cf5c470f.jpeg"
+  },
+  {
+    "revision": "1ab8a5ae88f792f7b9875bd1158cb5ef",
+    "url": "/static/media/photo.1ab8a5ae.jpg"
+  },
+  {
+    "revision": "33eff209e1af7fbd27de5e5aa7bfa9e9",
+    "url": "/static/media/spotify.33eff209.png"
+  },
+  {
+    "revision": "d4eed793587fad77b0d2edac5a9872ee",
+    "url": "/static/media/twitter.d4eed793.png"
+  },
+  {
+    "revision": "8fcf3e4a920782d00903f5e85accb11d",
+    "url": "/static/media/uvsq.8fcf3e4a.png"
+  },
+  {
+    "revision": "4727eafc914a5f27fbb5f682f385570a",
+    "url": "/static/media/whatsapp.4727eafc.png"
+  }
+]);
