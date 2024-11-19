eae64912ca19dbb9ec139f6f1f525e3e173b3d00
HawolyBa <hawolyba@gmail.com>
2024-11-19 20:46:26 +0100

    changed rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
