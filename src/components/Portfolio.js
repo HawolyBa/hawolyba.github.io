dbd41c4c5055b764bf4f4f8389a9b772c976840c
HawolyBa <hawolyba@gmail.com>
2024-12-12 20:54:43 +0100

    design corrections for portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -16,7 +16,7 @@ import buyi from '../images/buyi.PNG';
 
 const Portfolio = () => {
   return (
-    <div className="relative overflow-hidden bg-[#141414]">
+    <div className="relative overflow-hidden bg-[#141414] md:pt-0 pt-20">
       <div id="portfolio" className="relative h-full md:min-h-screen p-8 mt-12">
         <h2 className="text-3xl font-bold mb-8 text-center">Portfolio</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-4">
